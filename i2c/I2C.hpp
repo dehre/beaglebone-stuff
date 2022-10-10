1261f504297bfd5ff551e26d034d7216f6c3fe48
dehre <dehre.git@gmail.com>
2022-10-10 08:56:34 -0700

    clean up class I2C

--- i2c/I2C.hpp
+++ i2c/I2C.hpp
@@ -3,58 +3,59 @@
 #include <fcntl.h>
 #include <linux/i2c-dev.h>
 #include <linux/i2c.h>
-#include <stdio.h>
 #include <sys/ioctl.h>
 #include <unistd.h>
 
 #include <array>
-#include <sstream>
+#include <cstring>
 #include <stdexcept>
-#include <string>
 
 class I2C
 {
   public:
     I2C(int instance, int slaveAddress) : m_slaveAddress{slaveAddress}
     {
-        std::ostringstream pathToDev;
-        pathToDev << "/dev/i2c-" << instance;
-
-        if ((m_devFile = open(pathToDev.str().data(), O_RDWR)) < 0)
+        m_pathToDev.replace(m_pathToDev.find("{}"), 2, std::to_string(instance));
+        if ((m_fd = open(m_pathToDev.data(), O_RDWR)) < 0)
         {
-            throw std::invalid_argument("Failed to open the bus\n");
+            throw std::invalid_argument("Invalid i2c instance provided\n");
         };
 
-        if (ioctl(m_devFile, I2C_SLAVE, m_slaveAddress) < 0)
+        if (ioctl(m_fd, I2C_SLAVE, m_slaveAddress) < 0)
         {
-            throw std::invalid_argument("Failed to connect to the sensor\n");
+            throw std::invalid_argument("Invalid slave address provided\n");
         }
     }
 
     ~I2C()
     {
-        close(m_devFile);
+        close(m_fd);
     }
 
-    template <size_t size> void writei2c(std::array<uint8_t, size> buffer)
+    template <size_t size> void write(std::array<uint8_t, size> buffer)
     {
-        if (write(m_devFile, buffer.data(), buffer.size()) != 1)
+        if (::write(m_fd, buffer.data(), buffer.size()) != 1)
         {
-            throw std::invalid_argument("Failed to write 0xE3\n");
+            std::string errmsg{};
+            errmsg.append("Failed to write: ").append(std::strerror(errno));
+            throw std::runtime_error(errmsg);
         }
     }
 
-    template <size_t size> std::array<uint8_t, size> readi2c()
+    template <size_t size> std::array<uint8_t, size> read()
     {
         std::array<uint8_t, size> buffer{};
-        if (read(m_devFile, buffer.data(), buffer.size()) != buffer.size())
+        if (::read(m_fd, buffer.data(), buffer.size()) != buffer.size())
         {
-            throw std::invalid_argument("Failed to read raw data\n");
+            std::string errmsg{};
+            errmsg.append("Failed to read: ").append(std::strerror(errno));
+            throw std::runtime_error(errmsg);
         }
         return buffer;
     }
 
   private:
-    int m_devFile{};
+    std::string m_pathToDev{"/dev/i2c-{}"};
+    int m_fd{};
     int m_slaveAddress{};
 };
--- i2c/main.cpp
+++ i2c/main.cpp
@@ -1,63 +1,28 @@
-#include <fcntl.h>
-#include <linux/i2c-dev.h>
-#include <linux/i2c.h>
-#include <stdio.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
-
+#include "I2C.hpp"
 #include <cstdint>
 #include <iostream>
 
-#include "I2C.hpp"
+constexpr int g_i2cInstance{2};
+constexpr int g_sensorI2CAddress{0x40};
+
+constexpr int g_sensorTrigTempMeasurement{0xE3};
 
 int main()
 {
-    I2C sensor{I2C(2, 0x40)};
+    try
+    {
+        I2C sensor{I2C(g_i2cInstance, g_sensorI2CAddress)};
 
-    sensor.writei2c<1>({{0xE3}});
-    auto data_arr{sensor.readi2c<2>()};
+        sensor.write<1>({{g_sensorTrigTempMeasurement}});
+        auto rawTemperatureArr{sensor.read<2>()};
+        uint16_t rawTemperature{static_cast<uint16_t>((rawTemperatureArr[0] << 8) | (rawTemperatureArr[1] & 0xFC))};
 
-    uint16_t raw_reading{static_cast<uint16_t>((data_arr[0] << 8) | (data_arr[1] & 0xFC))};
-    double rawReadingDouble{static_cast<double>(raw_reading)};
-    double dst{-46.85 + 175.72 / 65536.0 * rawReadingDouble};
-
-    std::cout << "Everything went well! Got data: " << dst << '\n';
+        double temperature{-46.85 + 175.72 / 65536.0 * static_cast<double>(rawTemperature)};
+        std::cout << "Everything went well! Got data: " << temperature << '\n';
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+        // TODO LORIS: print usage
+    }
 }
-
-// int main()
-// {
-//     std::cout << "Starting the application\n";
-
-//     int file;
-//     if ((file = open("/dev/i2c-2", O_RDWR)) < 0)
-//     {
-//         perror("failed to open the bus\n");
-//         return 1;
-//     };
-//     if (ioctl(file, I2C_SLAVE, 0x40) < 0)
-//     {
-//         perror("Failed to connect to the sensor\n");
-//         return 1;
-//     }
-
-//     char writeBuffer[1]{0xE3};
-//     if (write(file, writeBuffer, 1) != 1)
-//     {
-//         perror("Failed to write 0xE3\n");
-//         return 1;
-//     }
-
-//     uint8_t data_arr[2]{};
-//     if (read(file, data_arr, 2) != 2)
-//     {
-//         perror("Failed to read raw data\n");
-//         return 1;
-//     }
-
-//     uint16_t raw_reading{(data_arr[0] << 8) | (data_arr[1] & 0xFC)};
-//     float dst{-46.85 + 175.72 / 65536 * (float)raw_reading};
-
-//     std::cout << "Everything went well! Got data: " << dst << '\n';
-//     close(file);
-//     return 0;
-// }
