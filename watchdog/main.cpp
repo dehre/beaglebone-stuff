2c41e4fe5980de0aadbaa30bd33e28bed0eaa620
dehre <dehre.git@gmail.com>
2022-10-26 01:59:29 -0700

    clean up main.cpp in project watchdog

--- watchdog/main.cpp
+++ watchdog/main.cpp
@@ -1,35 +1,31 @@
 #include "Watchdog.hpp"
+#include <cstdlib> // for EXIT_SUCCESS and EXIT_FAILURE
 #include <iostream>
-
-// TODO LORIS: do I really need sudo permissions?
-// TODO LORIS: refactor main do-while loop in a more c++ way?
+#include <string>
 
 int main()
 {
     try
     {
-        Watchdog wdg(20);
-        int state;
-        std::cout << "Press f to feed the dog, h to say hello, q to quit\n";
-        do
+        Watchdog wdg(30);
+        std::cout << "Type any string to feed the dog (timeout: 30s), use \"q\" or \"quit\" to quit\n";
+        std::string buf{};
+        while (true)
         {
-            state = getchar();
-            switch (state)
+            std::getline(std::cin, buf);
+            buf.erase(buf.find_last_not_of(" \n\r\t") + 1); /* trim trailing whitespaces */
+            if (buf == "q" || buf == "quit")
             {
-            case 'f':
-                std::cout << "Feed!\n";
-                wdg.feed();
-                break;
-            case 'h':
-                std::cout << "hello!\n";
                 break;
             }
-        } while (state != 'q');
+            std::cout << "[feed]\n";
+            wdg.feed();
+        }
     }
     catch (const std::exception &e)
     {
         std::cerr << e.what() << '\n';
-        return 1;
+        return EXIT_FAILURE;
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
